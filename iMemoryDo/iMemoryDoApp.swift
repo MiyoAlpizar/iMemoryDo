0268ee1ee86d9822f9103dadef9881c5bf43b4ad
Miyo Alpízar <miyo.naval@gmail.com>
2020-10-30 11:33:10 -0600

    Initial Commit

---
+++ iMemoryDo/iMemoryDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMemoryDoApp.swift
+//  iMemoryDo
+//
+//  Created by Miyo Alpízar on 30/10/20.
+//
+
+import SwiftUI
+
+@main
+struct iMemoryDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

06d10d53c8d465c556f75f92fe651d0057745d13
Miyo Alpízar <miyo.naval@gmail.com>
2020-11-10 15:04:54 -0600

    Carousel implemented

---
+++ iMemoryDo/Global/Extensions/Color+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Extensions.swift
+//  iMemoryDo
+//
+//  Created by Miyo Alpízar on 30/10/20.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    static let Accent = Color("Accent")
+    static let Accent2 = Color("Accent2")
+    static let Accent3 = Color("Accent3")
+    static let Accent4 = Color("Accent4")
+    static let Accent5 = Color("Accent5")
+    
+}
